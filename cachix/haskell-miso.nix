41da0890f971caacb13042c74bfe49f095fe9b2f
damianfral <huevofritopamojarpan@gmail.com>
2019-10-06 20:19:41 +0200

    Add cachix for Miso.

---
+++ cachix/haskell-miso.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://haskell-miso.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "haskell-miso.cachix.org-1:JU8k0o/s0G/LtD43BTkrIuLX8NfKktgq7MkgrCdtG6o="
+    ];
+    
+  };
+}
+    
